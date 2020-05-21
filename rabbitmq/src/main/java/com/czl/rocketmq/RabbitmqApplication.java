2c918035147fcb0bff0928c99624063f52520384
czlin <zelin.chen@aqara.com>
2020-05-21 18:01:28 +0800

    更改mq名为rabbit

--- rabbitmq/src/main/java/com/czl/rocketmq/RabbitmqApplication.java
+++ rabbitmq/src/main/java/com/czl/rocketmq/RabbitmqApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RocketmqApplication {
+public class RabbitmqApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RocketmqApplication.class, args);
+		SpringApplication.run(RabbitmqApplication.class, args);
 	}
 
 }
