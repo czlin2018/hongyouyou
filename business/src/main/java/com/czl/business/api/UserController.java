8c73149c038ad4c213bbd02fa8f0d75eb3538123
czlin <zelin.chen@aqara.com>
2020-05-21 11:56:51 +0800

    增加base工具类

--- business/src/main/java/com/czl/business/api/UserController.java
+++ business/src/main/java/com/czl/business/api/UserController.java
@@ -1,6 +1,10 @@
 package com.czl.business.api;
 
+import com.czl.base.response.ApiResponse;
+import com.czl.base.response.enums.ApiResponseEnum;
+import com.czl.base.util.BeanCopyUtil;
 import com.czl.base.util.DateUtil;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @创建日期: 2020-05-20
  * @创建时间: 15:28
  */
+@Validated
 @RestController
 public class UserController {
 
     @GetMapping("user/login")
-    public String login  ( ){
+    public ApiResponse login () {
+        System.out.println(BeanCopyUtil.copy(String.class, 1).getClass());
         System.out.println(DateUtil.dateToText());
-        return "success";
+        ApiResponseEnum fail = ApiResponseEnum.SUCCESS;
+        return ApiResponse.respond(fail);
     }
 }
