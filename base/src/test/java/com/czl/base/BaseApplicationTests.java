9b1ffc66f17479616f29ba0a07e1e69e412faaa9
czlin <zelin.chen@aqara.com>
2020-09-08 16:04:37 +0800

    add:引入注册中心服务器

--- base/src/test/java/com/czl/base/BaseApplicationTests.java
+++ base/src/test/java/com/czl/base/BaseApplicationTests.java
@@ -1,6 +1,6 @@
 package com.czl.base;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- eureka/src/test/java/com/czl/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/czl/eureka/EurekaApplicationTests.java
@@ -1,6 +1,6 @@
 package com.czl.eureka;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
