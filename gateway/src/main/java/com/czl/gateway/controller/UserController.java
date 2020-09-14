490e55791191dffdd8f8369eb70d1fec1aba7c25
czlin <zelin.chen@aqara.com>
2020-09-14 11:55:47 +0800

    add:feign实例代码

---
+++ gateway/src/main/java/com/czl/gateway/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.czl.gateway.controller;
+
+import com.czl.gateway.feign.PrivilegesFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @描述:
+ * @公司: lumi
+ * @author: 泽林
+ * @创建日期: 2020-09-14
+ * @创建时间: 11:46
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private PrivilegesFeign privilegesFeign;
+
+
+}
