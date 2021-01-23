353e0fcd724ffa2502c19c96889fcbd024c35a21
chenZeLin <953564227@qq.com>
2021-01-24 00:22:52 +0800

    ADD：增加算法题练习

---
+++ business/src/main/java/com/czl/business/算法练习/动态规划.java
@@ -0,0 +1,26 @@
+package com.czl.business.算法练习;
+
+
+/**
+ * @描述:
+ * @版权: Copyright (c) 2016-2018
+ * @公司: lumi
+ * @作者: 泽林
+ * @创建日期: 2021-01-24
+ * @创建时间: 0:07
+ */
+class 动态规划 {
+
+    public static void main(String[] args) {
+        爬楼梯(10);
+    }
+
+    /**
+     * 爬楼梯问题
+     */
+     static void 爬楼梯(int sumOfStairs){
+        System.out.println(sumOfStairs);
+    }
+
+
+}
